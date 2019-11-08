991df85862b9772ba5fbe4b095f30617950a0cea
mandaringit <compurain@gmail.com>
2019-11-08 11:50:18 +0900

    methods => computed로 변경

--- src/components/movies/MovieList.vue
+++ src/components/movies/MovieList.vue
@@ -7,7 +7,8 @@
     2-2. 드롭다운은 selectedGenreId data와 양방향바인딩(v-model)이 됩니다.
     2-3. 값 변경이 되면, 특정한 함수를 실행 해야합니다.
     -->
-    <select class="form-control" v-model="selectedGenreId" @change="getSelectedMovies">
+    <select class="form-control" v-model="selectedGenreId">
+      <option :value="'default'">전체보기</option>
       <option v-for="genre in genres" :key="genre.id" :value="genre.id">{{genre.name}}</option>
     </select>
     <!-- 1-3. 반복을 통해 호출하시오. 
@@ -16,12 +17,8 @@
 
     <!-- (나중에 마지막으로) selectedGenreId 값에 따른 분기를 해야 합니다.
     -->
-
-    <div v-if="selectedGenreId===''" class="row">
-      <MovieListItem v-for="movie in movies" :movie="movie" :key="movie.id" />
-    </div>
-    <div v-else class="row">
-      <MovieListItem v-for="movie in selectedMovies" :movie="movie" :key="movie.id" />
+    <div class="row">
+      <MovieListItem v-for="movie in computedMoviesByGenreId" :movie="movie" :key="movie.id" />
     </div>
   </div>
 </template>
@@ -35,13 +32,6 @@ export default {
   // 1-2. 아래에 등록 후
   components: {
     MovieListItem
-  },
-  data() {
-    return {
-      // 활용할 데이터를 정의하시오.
-      selectedGenreId: "",
-      selectedMovies: []
-    };
   },
   // 0. props 데이터를 받이 위하여 설정하시오.
   // genres와 movies 모두 타입은 Object이며, 필수입니다.
@@ -57,19 +47,50 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      // 활용할 데이터를 정의하시오.
+      selectedGenreId: "default"
+    };
+  },
   // 2-3.에서 이야기하는 특정한 함수는 selectedGenreId의 값이 변경될 때마다 호출 됩니다.
   // 드랍다운에서 장르를 선택하면, 해당 영화들만 보여주도록 구현 예정입니다.
   // 주의할 것은 직접 부모 컴포넌트의 데이터를 변경할 수 없다는 점입니다.
   // 완료 후
-  methods: {
-    getSelectedMovies() {
-      const genreId = this.selectedGenreId;
-
-      this.selectedMovies = this.movies.filter(
-        movie => movie.genre_id === genreId
+  computed: {
+    computedMoviesByGenreId() {
+      if (this.selectedGenreId === "default") return this.movies;
+      return this.movies.filter(
+        movie => movie.genre_id === this.selectedGenreId
       );
     }
   }
+
+  // 메서드 사용
+  // methods: {
+  //   getSelectedMovies() {
+  //     const genreId = this.selectedGenreId;
+
+  //     this.selectedMovies = this.movies.filter(
+  //       movie => movie.genre_id === genreId
+  //     );
+  //   }
+  // },
+
+  // watch 사용 (초기엔 변화 없음)
+  // watch: {
+  //   selectedGenreId: {
+  //     handler(selectedGenreId) {
+  //       if (selectedGenreId === "default") {
+  //         this.selectedMovies = this.movies;
+  //       } else {
+  //         this.selectedMovies = this.movies.filter(
+  //           movie => movie.genre_id === selectedGenreId
+  //         );
+  //       }
+  //     }
+  //   }
+  // },
 };
 </script>
 
